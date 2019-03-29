1ee19fa1c9e7ccb3fad7c472971d1e7bb3598876
Amin <amin69y@gmail.com>
2019-03-30 01:23:02 +0430

    representation

--- streaming/auto_rep.py
+++ streaming/auto_rep.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from .utiles import round_to_even
 from .streams import Streams
 from .rep import Representation
@@ -7,22 +5,25 @@ from .rep import Representation
 
 def get_kilo_bit_rates(kilo_bit_rate, count):
     kilo_bites = []
+
     while count:
         kilo_bit_rate = int(kilo_bit_rate/1.3)
         kilo_bites.append(kilo_bit_rate)
         count -= 1
+
     return kilo_bites
 
 
 class AutoRepresentation:
-    def __init__(self, ffprob, heights):
+    def __init__(self, stream, heights):
+        if not isinstance(stream, Streams):
+            raise TypeError('The input must be instance of Streams')
+        self.video = stream.video()
+
         if heights is None:
             heights = [2160, 1080, 720, 480, 240, 144]
         self.heights = heights
-        if not isinstance(ffprob, Streams):
-            raise TypeError('The input must be instance of Streams')
-        self.video = ffprob.video()
-
+        
     def generate(self):
         width, height, ratio = self.dimension()
         kilo_bitrate = round(int(self.video['bit_rate']) / 1024)
@@ -45,4 +46,3 @@ class AutoRepresentation:
         ratio = width / height
 
         return width, height, ratio
-
