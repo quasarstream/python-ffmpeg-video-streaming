a13292d41a74d3c22311c031e10caa1d84a094b6
Amin <amin69y@gmail.com>
2020-04-28 21:39:03 +0430

    Bug fixes #14

--- ffmpeg_streaming/_command_builder.py
+++ ffmpeg_streaming/_command_builder.py
@@ -16,40 +16,55 @@ from ._utiles import cnv_options_to_args, get_path_info, clean_args
 
 
 def _stream2file(stream2file):
-    args = {'c': 'copy'}
+    """
+    @TODO: add documentation
+    """
+    args = stream2file.format.all
+    args.update({'c': 'copy'})
     args.update(stream2file.options)
 
     return cnv_options_to_args(args) + [stream2file.output_]
 
 
+def _get_audio_bitrate(rep, index: int = None):
+    """
+    @TODO: add documentation
+    """
+    if rep.bitrate.audio_ is not None and rep.bitrate.audio_ != 0:
+        opt = 'b:a' if index is None else 'b:a:' + str(index)
+        return {opt: rep.bitrate.audio}
+
+    return {}
+
+
 def _get_dash_stream(key, rep):
+    """
+    @TODO: add documentation
+    """
     args = {
-        'map':              '0',
-        'b:v:' + str(key):   rep.bitrate.normalize_video(),
-        'b:a:' + str(key):   rep.bitrate.audio,
-        's:v:' + str(key):   rep.size.normalize
+        'map':              0,
+        's:v:' + str(key):   rep.size.normalize,
+        'b:v:' + str(key):   rep.bitrate.normalize_video()
     }
+    args.update(_get_audio_bitrate(rep, key))
     return cnv_options_to_args(args)
 
 
 def _dash(dash):
+    """
+    @TODO: add documentation
+    """
     dirname, name = get_path_info(dash.output_)
-    init_args = {
-        'c:v':              dash.format.video,
-        'c:a':              dash.format.audio,
-        'bf':              '1',
-        'keyint_min':      '120',
-        'g':               '120',
-        'sc_threshold':    '0',
-        'b_strategy':      '0',
-        'use_timeline':    '1',
-        'use_template':    '1',
+    _args = dash.format.all
+    _args.update({
+        'use_timeline':    1,
+        'use_template':    1,
         'init_seg_name':   name + '_init_$RepresentationID$.$ext$',
         "media_seg_name":  name + '_chunk_$RepresentationID$_$Number%05d$.$ext$',
         'f':               'dash'
-    }
-    init_args.update(dash.options)
-    args = cnv_options_to_args(init_args)
+    })
+    _args.update(dash.options)
+    args = cnv_options_to_args(_args)
 
     for key, rep in enumerate(dash.reps):
         args += _get_dash_stream(key, rep)
@@ -58,28 +73,28 @@ def _dash(dash):
 
 
 def _hls_seg_ext(hls):
+    """
+    @TODO: add documentation
+    """
     return 'm4s' if hls.options.get('hls_segment_type', '') == 'fmp4' else 'ts'
 
 
 def _get_hls_stream(hls, rep, dirname, name):
-    args = {
-        'c:v':                      hls.format.video,
-        'c:a':                      hls.format.audio,
-        's:v':                      rep.size.normalize,
-        'crf':                      '20',
-        'sc_threshold':             '0',
-        'g':                        '48',
-        'keyint_min':               '48',
-        'hls_list_size':            '0',
-        'hls_time':                 '10',
-        'hls_allow_cache':          '1',
-        'b:v':                      rep.bitrate.normalize_video(),
-        'b:a':                      rep.bitrate.audio,
-        'maxrate':                  rep.bitrate.max_rate,
+    """
+    @TODO: add documentation
+    """
+    args = hls.format.all
+    args.update({
+        'hls_list_size':            0,
+        'hls_time':                 10,
+        'hls_allow_cache':          1,
         'hls_segment_filename':     dirname + "/" + name + "_" + str(rep.size.height) + "p_%04d." + _hls_seg_ext(hls),
         'hls_fmp4_init_filename':   name + "_" + str(rep.size.height) + "p_init.mp4",
-        'strict':                   '-2'
-    }
+        's:v':                      rep.size.normalize,
+        'b:v':                      rep.bitrate.normalize_video()
+    })
+    args.update(_get_audio_bitrate(rep))
+    args.update({'strict': '-2'})
     args.update(hls.options)
 
     return cnv_options_to_args(args) + [dirname + "/" + name + "_" + str(rep.size.height) + "p.m3u8"]
