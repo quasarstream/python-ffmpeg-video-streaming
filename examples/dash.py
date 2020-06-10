6b3fa6d15fdb2185eaeb69707e5bd85797c6053c
Amin <amin69y@gmail.com>
2020-06-10 22:51:52 +0430

    Bug fixes and other minor improvements

--- examples/dash.py
+++ examples/dash.py
@@ -35,12 +35,17 @@ def time_left(time_, total):
     return time_left
 
 
-def monitor(ffmpeg, duration, time_):
+def monitor(ffmpeg, duration, time_, process):
     # You can update a field in your database or log it to a file
     # You can also create a socket connection and show a progress bar to users
-    # logging.info(ffmpeg)
+    # logging.info(ffmpeg) or print(ffmpeg)
+
+    # if "something happened":
+    #     process.terminate()
+
     per = round(time_ / duration * 100)
-    sys.stdout.write("\rTranscoding...(%s%%) %s [%s%s]" % (per, time_left(time_, duration), '#' * per, '-' * (100 - per)))
+    sys.stdout.write(
+        "\rTranscoding...(%s%%) %s [%s%s]" % (per, time_left(time_, duration), '#' * per, '-' * (100 - per)))
     sys.stdout.flush()
 
 
@@ -49,12 +54,19 @@ def main():
 
     parser.add_argument('-i', '--input', required=True, help='The path to the video file (required).')
     parser.add_argument('-o', '--output', default=None, help='The output to write files.')
+
+    parser.add_argument('-hls', '--hls_output', default=False, help='publish hls playlists')
+
     args = parser.parse_args()
 
     video = ffmpeg_streaming.input(args.input)
 
     dash = video.dash(Formats.h264())
     dash.auto_generate_representations()
+
+    if args.hls_output:
+        dash.generate_hls_playlist()
+
     dash.output(args.output, monitor=monitor)
 
 
