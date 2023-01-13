e21a81a4d6258ed10b82a54ff166db5547bfaefc
Diego Blanco <diego.blanco@treitos.com>
2023-01-13 18:16:51 +0100

    Fixed the place where the adaptation_set parameter is placed

--- ffmpeg_streaming/_command_builder.py
+++ ffmpeg_streaming/_command_builder.py
@@ -54,7 +54,6 @@ def _get_dash_stream(key, rep):
         'map': 0,
         f's:v:{str(key)}': rep.size,
         f'b:v:{str(key)}': rep.bitrate.calc_video(),
-        'adaptation_sets': 'id=0,streams=v id=1,streams=a',
     }
 
     args.update(_get_audio_bitrate(rep, key))
@@ -69,11 +68,12 @@ def _dash(dash):
     dirname, name = get_path_info(dash.output_)
     _args = dash.format.all
     _args.update({
-        'use_timeline':     USE_TIMELINE,
-        'use_template':     USE_TEMPLATE,
-        'init_seg_name':    '{}_init_$RepresentationID$.$ext$'.format(name),
-        "media_seg_name":   '{}_chunk_$RepresentationID$_$Number%05d$.$ext$'.format(name),
-        'f': 'dash'
+        'use_timeline': USE_TIMELINE,
+        'use_template': USE_TEMPLATE,
+        'init_seg_name': '{}_init_$RepresentationID$.$ext$'.format(name),
+        "media_seg_name": '{}_chunk_$RepresentationID$_$Number%05d$.$ext$'.format(name),
+        'f': 'dash',
+        'adaptation_sets': 'id=0,streams=v id=1,streams=a',
     })
     _args.update(dash.options)
     args = cnv_options_to_args(_args)
