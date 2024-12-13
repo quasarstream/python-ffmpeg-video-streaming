20025d06666c986de5cf776faaa64393931292c5
Robert Colfin <rcolfin@hotmail.com>
2024-12-13 15:02:27 -0500

    including __version__ as part of the package.

--- ffmpeg_streaming/__init__.py
+++ ffmpeg_streaming/__init__.py
@@ -1,8 +1,17 @@
-from ._input import *
-from ._format import *
-from ._reperesentation import *
-from ._media_property import *
+import importlib.metadata
+
 from ._clouds import *
+from ._format import *
+from ._input import *
+from ._media_property import *
+from ._reperesentation import *
 from .ffprobe import *
 
-__all__ = _input.__all__ + _format.__all__ + _reperesentation.__all__ + _media_property.__all__ + _clouds.__all__ + ffprobe.__all__
+try:
+    __version__: str = importlib.metadata.version("python-ffmpeg-video-streaming")
+except importlib.metadata.PackageNotFoundError:
+    # package is not installed
+    __version__ = None
+
+
+__all__ = ["__version__"] + _input.__all__ + _format.__all__ + _reperesentation.__all__ + _media_property.__all__ + _clouds.__all__ + ffprobe.__all__
